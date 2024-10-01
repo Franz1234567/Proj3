83f8adca1c76a792ab74ae7dd315734f766aeaae
Barnouin <barnouin@eurecom.fr>
2024-10-01 11:01:40 +0000

    pi_controller on progress + test  units

---
+++ include/pi_controller.h
@@ -0,0 +1,15 @@
+// #ifndef PI_CONTROLLER_H
+// #define PI_CONTROLLER_H
+
+// class PI_Controller : public P_controller {
+// private:
+//     double Ti;
+//     double T;
+//     double sum_error;
+
+// public:
+//     PI_Controller(double Kp, double Ti, double T);
+//     double update(double ref, double actual) override;
+// };
+
+// #endif
---
+++ lib/pi_controller/p_controller.h
@@ -0,0 +1,12 @@
+#ifndef P_CONTROLLER_H
+#define P_CONTROLLER_H
+
+class P_controller{
+    public:
+        P_controller(double Kp);
+        virtual double update(double ref, double actual);
+    private:
+        double Kp_priv;
+};
+
+#endif
